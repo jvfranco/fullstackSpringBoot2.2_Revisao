98490959fb77332fdaa1b5e18fb252c9a534ee4e
João Victor <joaovictorfrancorosa@gmail.com>
2021-04-15 22:44:12 -0300

    Revisao de Conceitos

--- src/main/java/com/github/jvfranco/VendasApplication.java
+++ src/main/java/com/github/jvfranco/VendasApplication.java
@@ -1,10 +1,27 @@
 package com.github.jvfranco;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+//@ComponentScan(basePackages = {"com.github.jvfranco", "com.github.jvfranco"})
 public class VendasApplication {
+
+    //@Autowired
+    //@Qualifier("applicationName")
+	@Value("${application.name}")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return this.applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
